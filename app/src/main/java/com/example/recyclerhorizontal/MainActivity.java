189fa1b807618e5bf1cbafca092cbeb707fc4114
drosalBalmes <drosal@jaumebalmes.net>
2023-02-28 10:56:41 +0100

    Added scale center item

--- app/src/main/java/com/example/recyclerhorizontal/MainActivity.java
+++ app/src/main/java/com/example/recyclerhorizontal/MainActivity.java
@@ -7,13 +7,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.example.recyclerhorizontal.LayoutManager.ScaleCenterItemLayoutManager;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView rv1;
     RecyclerView rv2;
     ArrayList<Skin> dataSource;
-    LinearLayoutManager linearLayoutManager;
+    ScaleCenterItemLayoutManager linearLayoutManager;
     AdapterSkin adapter;
 
     @SuppressLint("MissingInflatedId")
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         dataSource.add(new Skin("Third", R.drawable.ic_launcher_background));
         dataSource.add(new Skin("xd", R.drawable.ic_launcher_background));
 
-        linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
+        linearLayoutManager = new ScaleCenterItemLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         adapter = new AdapterSkin(dataSource);
         rv1.setLayoutManager(linearLayoutManager);
         rv1.setAdapter(adapter);
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         dataSource.add(new Skin("Luffy",R.drawable.luffy_xd));
         dataSource.add(new Skin("5",R.drawable.ic_launcher_background));
 
-        linearLayoutManager = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        linearLayoutManager = new ScaleCenterItemLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
         adapter = new AdapterSkin(dataSource);
         rv2.setLayoutManager(linearLayoutManager);
         rv2.setAdapter(adapter);
