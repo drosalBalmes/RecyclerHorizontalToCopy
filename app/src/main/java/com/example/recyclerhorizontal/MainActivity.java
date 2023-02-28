a0aa50e676c7ee3e656449e7a20f666c60e0a5b2
drosalBalmes <drosal@jaumebalmes.net>
2023-02-28 10:46:11 +0100

    Added Second recycler same activity

--- app/src/main/java/com/example/recyclerhorizontal/MainActivity.java
+++ app/src/main/java/com/example/recyclerhorizontal/MainActivity.java
@@ -1,39 +1,53 @@
 package com.example.recyclerhorizontal;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView rv;
+    RecyclerView rv1;
+    RecyclerView rv2;
     ArrayList<Skin> dataSource;
     LinearLayoutManager linearLayoutManager;
     AdapterSkin adapter;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rv = findViewById(R.id.horizontalRv);
+        rv1 = findViewById(R.id.horizontalRv);
+        rv2 = findViewById(R.id.horizontalRv2);
+
+        //First recycler
         dataSource = new ArrayList<>();
-        dataSource.add(new Skin("Hola", R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("First", R.drawable.ic_launcher_background));
         dataSource.add(new Skin("Luffy", R.drawable.luffy_xd));
-        dataSource.add(new Skin("Hola", R.drawable.ic_launcher_background));
-        dataSource.add(new Skin("Hola", R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("Third", R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("xd", R.drawable.ic_launcher_background));
 
         linearLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
         adapter = new AdapterSkin(dataSource);
-        rv.setLayoutManager(linearLayoutManager);
-        rv.setAdapter(adapter);
+        rv1.setLayoutManager(linearLayoutManager);
+        rv1.setAdapter(adapter);
+
+
+        //Second recycler
+        dataSource = new ArrayList<>();
+        dataSource.add(new Skin("1",R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("2",R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("3",R.drawable.ic_launcher_background));
+        dataSource.add(new Skin("Luffy",R.drawable.luffy_xd));
+        dataSource.add(new Skin("5",R.drawable.ic_launcher_background));
+
+        linearLayoutManager = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        adapter = new AdapterSkin(dataSource);
+        rv2.setLayoutManager(linearLayoutManager);
+        rv2.setAdapter(adapter);
     }
 }
